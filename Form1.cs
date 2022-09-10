96711a387566f4b846443696fa9d3e72e9fc0b7f
Mr_Dyeong <dheyeongtchalla@gmail.com>
2022-09-11 08:26:38 +0900

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        GoogleDriveClass Drive = new GoogleDriveClass();
+        private static int numThreads = 10;
+        private static string pipeName = "GoogleBridge";
+        static Thread[] servers;
+        public Form1()
+        {
+            InitializeComponent();
+            PipesCreate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void PipesCreate()
+        {
+            int i;
+            servers = new Thread[numThreads];
+
+            for (i = 0; i < numThreads; i++)
+            {
+                servers[i] = new Thread(ServerThread);
+                servers[i].Start();
+            }
+        }
+
+        private void ServerThread()
+        {
+            NamedPipeServerStream pipeServer =
+                new NamedPipeServerStream(pipeName, PipeDirection.InOut, numThreads, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            // Wait for a client to connect
+            AsyncCallback asyn_connected = new AsyncCallback(Connected);
+            try
+            {
+                pipeServer.BeginWaitForConnection(asyn_connected, pipeServer);
+            }
+            catch (Exception)
+            {
+                servers[threadId].Suspend();
+                servers[threadId].Start();
+            }
+        }
+
+        private void Connected(IAsyncResult pipe)
+        {
+            if (!pipe.IsCompleted)
+                return;
+            bool exit = false;
+            try
+            {
+
+                NamedPipeServerStream pipeServer = (NamedPipeServerStream)pipe.AsyncState;
+                try
+                {
+                    if (!pipeServer.IsConnected)
+                        pipeServer.WaitForConnection();
+                }
+                catch (IOException)
+                {
+                    AsyncCallback asyn_connected = new AsyncCallback(Connected);
+                    pipeServer.Dispose();
+                    pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut, numThreads, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
+                    pipeServer.BeginWaitForConnection(asyn_connected, pipeServer);
+                    return;
+                }
+                while (!exit && pipeServer.IsConnected)
+                {
+                    // Read the request from the client. Once the client has
+                    // written to the pipe its security token will be available.
+
+                    while (pipeServer.IsConnected)
+                    {
+                        if (!ReadMessage(pipeServer))
+                        {
+                            exit = true;
+                            break;
+                        }
+                    }
+                    //Wait for a client to connect
+                    AsyncCallback asyn_connected = new AsyncCallback(Connected);
+                    pipeServer.Disconnect();
+                    pipeServer.BeginWaitForConnection(asyn_connected, pipeServer);
+                    break;
+                }
+            }
+            finally
+            {
+                exit = true;
+            }
+
+        }
+
+        private bool ReadMessage(PipeStream pipe)
+        {
+            if (!pipe.IsConnected)
+                return false;
+
+            byte[] arr_read = new byte[1024];
+            string message = null;
+            int length;
+            do
+            {
+                length = pipe.Read(arr_read, 0, 1024);
+                if (length > 0)
+                    message += Encoding.Default.GetString(arr_read, 0, length);
+            } while (length >= 1024 && pipe.IsConnected);
+            if (message == null)
+                return true;
+            message = message.Substring(0, message.Length - 1);
+
+            if (message.Trim() == "Close")
+                return false;
+
+            string result = null;
+            string[] separates = { ";" };
+            string[] arr_message = message.Split(separates, StringSplitOptions.RemoveEmptyEntries);
+            if (arr_message[0].Trim() == "Read")
+            {
+                //Action action = () =>
+                //    {
+                try
+                {
+                    result = Drive.FileRead(Drive.GetFileId(arr_message[1].Trim() + GoogleDriveClass.extension));
+                }
+                catch (Exception e)
+                {
+                    result = "Error " + e.ToString();
+                    Drive.Authorize();
+                }
+                //    };
+                //if (InvokeRequired) Invoke(action); else action();
+                return WriteMessage(pipe, result);
+            }
+
+            if (arr_message[0].Trim() == "Write")
+            {
+                try
+                {
+                    result = (Drive.FileUpdate(arr_message[1].Trim() + GoogleDriveClass.extension, arr_message[2].Trim()) ? "Ok" : "Error");
+                }
+                catch (Exception e)
+                {
+                    result = "Error " + e.ToString();
+                    Drive.Authorize();
+                }
+
+                return WriteMessage(pipe, result);
+            }
+            return true;
+        }
+
+        private bool WriteMessage(PipeStream pipe, string message)
+        {
+            if (!pipe.IsConnected)
+                return false;
+            if (message == null || message.Count() == 0)
+                message = "Empty";
+            byte[] arr_bytes = Encoding.Default.GetBytes(message);
+            try
+            {
+                pipe.Flush();
+                pipe.Write(arr_bytes, 0, arr_bytes.Count());
+                pipe.Flush();
+            }
+            catch (IOException)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,264 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using File = Google.Apis.Drive.v3.Data.File;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class GoogleDriveClass
+    {
+        static string[] Scopes = { DriveService.Scope.DriveFile };  //Array for working with class
+        static string ApplicationName = "Google Drive Bridge";      //Application name
+        public static UserCredential credential = null;             //Authorization keys
+        public static string extension = ".gdb";                    //Extension for saved files
+
+
+        public bool Authorize()
+        {
+            using (System.IO.FileStream stream =
+                     new System.IO.FileStream("client-secret.json", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                try
+                {
+                    string credPath = System.Environment.CurrentDirectory.ToString();
+                    credPath = System.IO.Path.Combine(credPath, "drive-bridge.json");
+
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        GoogleDriveClass.Scopes,
+                        "exampletrade@gmail.com",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                }
+                catch (Exception)
+                {
+                    credential = null;
+                }
+
+            }
+            return (credential != null);
+        }
+
+        public string GetFileId(string name)
+        {
+            string result = null;
+            IList<File> files = GetFileList();
+
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Name == name)
+                    {
+                        result = file.Id;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        public bool FileCreate(string name, string value, out string id)
+        {
+            bool result = false;
+            id = null;
+            if (credential == null)
+                this.Authorize();
+            if (credential == null)
+            {
+                return result;
+            }
+            using (var service = new Google.Apis.Drive.v3.DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            }))
+            {
+                var body = new File();
+                body.Name = name;
+                body.MimeType = "text/json";
+                body.ViewersCanCopyContent = true;
+
+                byte[] byteArray = Encoding.Default.GetBytes(value);
+                using (var stream = new System.IO.MemoryStream(byteArray))
+                {
+                    Google.Apis.Drive.v3.FilesResource.CreateMediaUpload request = service.Files.Create(body, stream, body.MimeType);
+                    if (request.Upload().Exception == null)
+                    { id = request.ResponseBody.Id; result = true; }
+                }
+            }
+            return result;
+        }
+
+        public bool FileUpdate(string name, string value)
+        {
+            bool result = false;
+            if (credential == null)
+                this.Authorize();
+            if (credential == null)
+            {
+                return result;
+            }
+
+            string new_id;
+            if (FileCreate(name, value, out new_id))
+            {
+                IList<File> files = GetFileList();
+                if (files != null && files.Count > 0)
+                {
+                    result = true;
+                    try
+                    {
+                        using (var service = new DriveService(new BaseClientService.Initializer()
+                        {
+                            HttpClientInitializer = credential,
+                            ApplicationName = ApplicationName,
+                        }))
+                        {
+                            foreach (var file in files)
+                            {
+                                if (file.Name == name && file.Id != new_id)
+                                {
+                                    try
+                                    {
+                                        Google.Apis.Drive.v3.FilesResource.DeleteRequest request = service.Files.Delete(file.Id);
+                                        string res = request.Execute();
+                                    }
+                                    catch (Exception)
+                                    {
+                                        continue;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return result;
+                    }
+
+                }
+
+            }
+            return result;
+        }
+
+        public string FileRead(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return ("Errore. File not found");
+            }
+            bool result = false;
+            string value = null;
+            if (credential == null)
+                this.Authorize();
+            if (credential != null)
+            {
+                using (var service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = ApplicationName,
+                }))
+                {
+                    Google.Apis.Drive.v3.FilesResource.GetRequest request = service.Files.Get(id);
+                    using (var stream = new MemoryStream())
+                    {
+                        request.MediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
+                        {
+                            if (progress.Status == DownloadStatus.Completed)
+                                result = true;
+                        };
+                        request.Download(stream);
+                        if (result)
+                        {
+                            int start = 0;
+                            int count = (int)stream.Length;
+                            value = Encoding.Default.GetString(stream.GetBuffer(), start, count);
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+
+        public IList<File> GetFileList()
+        {
+            IList<File> result = null;
+            //Control control = new Control();
+            if (credential == null)
+                this.Authorize();
+            if (credential == null)
+            {
+                return result;
+            }
+            // Create Drive API service.
+            using (Google.Apis.Drive.v3.DriveService service = new Google.Apis.Drive.v3.DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            }))
+            {
+                try
+                {
+                    // Define parameters of request.
+                    FilesResource.ListRequest listRequest = service.Files.List();
+                    listRequest.PageSize = 1000;
+                    listRequest.Fields = "nextPageToken, files(id, name, size)";
+
+                    // List files.
+                    result = listRequest.Execute().Files;
+                }
+                catch (Exception e)
+                {
+                    string mess = e.ToString();
+                    return null;
+                }
+            }
+            return result;
+        }
+
+        public string GetFileListJSON()
+        {
+            IList<File> files = GetFileList();
+            string result = null;
+            if (files != null && files.Count > 0)
+            {
+                result = "{ FileList { ";
+                foreach (var file in files)
+                {
+                    if (file.Name.EndsWith(extension))
+                        result += "{ " + file.Name.Replace(extension, "").Trim() + "; " + file.Id.Trim() + " }";
+                }
+                result += " }";
+            }
+            return result;
+        }
+    }
+}
